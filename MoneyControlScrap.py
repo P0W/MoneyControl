2b4f6571e70f3727d468e57498eb97ac30c18c44
Prashant Srivastava <powprashant@gmail.com>
2016-11-28 18:07:17 +0530

    Update MoneyControlScrap.py
    Updates to write data into sql database

--- MoneyControlScrap.py
+++ MoneyControlScrap.py
@@ -1,79 +1,173 @@
+
+## Grab stocks data from MoneyControl.com
+
+##1. Zero Debt
+##2. More percentage of Cash Flow to Market Cap 
+##3. Zero ShareHolder Stake Pledge
+##4. Large Profit
+##5. Less Competition
+
+
 from bs4 import BeautifulSoup
 from urllib.request import *
 import re
+import sqlite3
 
 class UrlOpener( object ):
     def __init__(self, url):
         self.url = url
 
     def contents(self):
-        return urlopen(self.url)
+        return BeautifulSoup( urlopen(self.url), "html.parser" )
 
 
-test_sufix ='computers-software'
-moneyControlSite = UrlOpener( r'http://www.moneycontrol.com/stocks/sectors/'  )
+class StockDB( object ):
+    def __init__(self):
+        self.connector = sqlite3.connect('stocks.db')
+        self.cursor = self.connector.cursor()
 
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS
+                 STOCKSDATA
+                (
+                Company TEXT PRIMARY KEY NOT NULL,
+                Market_Cap REAL NOT NULL,
+                Total_Debt REAL NOT NULL,
+                Total_Asset REAL NOT NULL,
+                Cash_Flow REAL NOT NULL,
+                Cash_to_Market REAL NOT NULL,
+                Pomoters_Stake_Pledged TEXT NOT NULL
+                );''')
+        
+        self.connector.commit()
 
-soup = BeautifulSoup(moneyControlSite.contents(), "html.parser")
+    def writeData(self, values):
+        try:
+            self.cursor.execute("""
+            INSERT INTO
+                STOCKSDATA
+                ( Company, Market_Cap, Total_Debt, Total_Asset, Cash_Flow, Cash_to_Market, Pomoters_Stake_Pledged )
+                VALUES( ?,?,?,?,?,?,? );
+                """, values)
 
-##scrapPattern = [
-##  r'/stocks/sectors/.+\.html$'
-##  r'/stockpricequote/.+\.html$' 
-##]
+            self.connector.commit()
+        except sqlite3.IntegrityError:
+            print ('      Already Exists Skipped...')
+
+    def displayData(self, sortBY = 'Total_Debt', order = '' ):
+        for row in self.cursor.execute('SELECT * FROM STOCKSDATA ORDER BY %s %s' % (sortBY, order ) ):
+            print (row)
+
+    def companyList(self):
+        res = []
+        for row in self.cursor.execute('SELECT Company FROM STOCKSDATA ORDER BY Company'):
+            res.append( row[0] )
+        return res
+    
+    def __del__(self):
+        print ('Closing Database')
+        self.cursor.close()
+        self.connector.close()
 
 def ReadCashFlow( url ):
-    soup = BeautifulSoup( UrlOpener( url ).contents(), "html.parser")
-    return soup.findAll(text='Closing Cash & Cash Equivalents')[0].findNext().get_text()
+    soup =  UrlOpener( url ).contents()
+    try:
+        return soup.find(text='Closing Cash & Cash Equivalents').findNext().get_text()
+    except AttributeError:
+        return '0.0'
+    except Exception as e:
+        print (str(e))
+        return '0.0'
 
-scrapPattern_0 = soup("a", {  'href':re.compile(r'/stocks/sectors/%s\.html$' % test_sufix) })
-visited = {}
 
-
-for sites in scrapPattern_0:
-    print ( 'Listing all companies from %s Sector' % sites.get_text() )
-    soup = BeautifulSoup( UrlOpener( sites['href'] ).contents(), "html.parser")
+def isPromotersStakePledged( url ):
+    # <table width="100%" border="0" class="b_12 dvdtbl" cellspacing="0" cellpadding="0">
+    soup =  UrlOpener( url ).contents()
+    try:
+        
+        rows = soup.find("table", {"class":"b_12 dvdtbl"} ).find_all("tr")
+        for row in rows[2:]:
+            cells = row.find_all("td")
+            for cell in cells[-1:]:
+                if  '-' not in cell.get_text():
+                    print (cell.get_text())
+                    return True
+    except AttributeError:
+        return True
+    except Exception as e:
+        print (str(e))
+        return False    
+    return False
     
-    scrapPattern_1 = soup("a", {  'href':re.compile(r'/stockpricequote/.+/.+') })
 
-    count = 0
+def GrabStocks():
+    DB = StockDB()
+    test_sufix = '.+' # 'auto-cars-jeeps' # 'chemicals' # 'cement-major' # 'computers-software' # 'auto-cars-jeeps' #
+    soup = UrlOpener( r'http://www.moneycontrol.com/stocks/sectors/'  ).contents()
+    scrapPattern_0 = soup("a", {  'href':re.compile(r'/stocks/sectors/%s\.html$' % test_sufix) })
+    visited = {}
+    companyList = DB.companyList()
+    
+    for sites in scrapPattern_0:
+        
+        soup = UrlOpener( sites['href'] ).contents()
+        
+        scrapPattern_1 = soup("a", {  'href':re.compile(r'/stockpricequote/.+/.+') })
+        
+        if scrapPattern_1:
 
-    for stocks in scrapPattern_1:
-        soup = BeautifulSoup( UrlOpener( r'http://www.moneycontrol.com/%s' % stocks['href'] ).contents(), "html.parser")
-        cashflowUrl = r'http://www.moneycontrol.com/%s' % ( soup.findAll(text='Cash Flow')[1].findPrevious()['href'] )
-         
+            print ( 'Listing all companies from %s Sector' % sites.get_text() )
+        for stocks in scrapPattern_1:
+
+            compName = stocks.get_text()
+            
+            if compName in visited  :continue
+            elif compName in companyList: continue
+            else: visited[ stocks.get_text() ] =''
+
+            print ( '--- %s' % stocks.get_text() )
+            
+            soup = UrlOpener( r'http://www.moneycontrol.com/%s' % stocks['href'] ).contents()
+
+            try:
+                cashflowUrl = r'http://www.moneycontrol.com/%s' % ( soup.findAll(text='Cash Flow')[1].findPrevious()['href'] )
+                Cash_Flow =  float(ReadCashFlow( cashflowUrl ).replace(',', '' ))
+            except:
+                Cash_Flow = 0.0
+
+            try:
+                sharesPledgedByPromotersUrl = r'http://www.moneycontrol.com/%s' % soup.find(text='Shares Pledged by Promoters').findParent()['href']
+                isStakePledged = isPromotersStakePledged( sharesPledgedByPromotersUrl )
+            except:
+                isStakePledged = True
+
+            try:      
+                Market_Cap = soup.find(text='MARKET CAP (Rs Cr)').findNext().get_text(),
+                Market_Cap = float(Market_Cap[0].replace(',',''))
+                Total_Debt =  float( soup.find(text='Total Debt').findNext().get_text().replace(',', '' ) )
+                Total_Asset = float(  soup.findAll(text='Total Assets')[1].findNext().get_text().replace(',', '' ) )
+                Cash_to_Market_cap_ratio = 100.0*Cash_Flow  /  Market_Cap
+            except:
+                Market_Cap = 1.0
+                Total_Debt = 0.0
+                Total_Asset = 0.0
+                Cash_to_Market_cap_ratio = 0.0
+
+            values = [
+            stocks.get_text(),
+             Market_Cap ,
+             Total_Debt, ## Total Debt
+             Total_Asset, ## Total Asset
+             Cash_Flow,
+             Cash_to_Market_cap_ratio,
+             '%s' % isStakePledged
+            ]        
+            DB.writeData( values )
+       
+
+def Display( sortBy = 'Total_Debt', order ='' ):            
+    StockDB().displayData( sortBy, order )
+
         
 
-        if stocks.get_text() in visited:continue
-        else: visited[ stocks.get_text() ] =''
-        
-        #scrapPattern_2 = soup("div", {'id':'findet_11' } )
-        #scrapPattern_3 = soup("div", {'class':'FR gD_12' } ) #<div class="FR gD_12">453,365.91</div>
-        #print ( scrapPattern_3[0].get_text() )
-        if True: #scrapPattern_2 and scrapPattern_3 :
-            count += 1
-            Market_Cap = soup.findAll(text='MARKET CAP (Rs Cr)')[0].findNext().get_text()[0],
-            Cash_Flow =  ReadCashFlow( cashflowUrl )
-            Cash_to_Market_cap_ratio = 0 #100.0*float(Cash_Flow.replace(',', '' )/ float( Market_Cap.replace(',','') ) )
-            result = {
-            'Count': count,
-            'Company':stocks.get_text(),
-            'Market_Cap': Market_Cap ,
-            'Total_Debt': soup.findAll(text='Total Debt')[0].findNext().get_text(), ## Total Debt
-            'Total_Asset':soup.findAll(text='Total Assets')[1].findNext().get_text(), ## Total Asset
-            'Cash_Flow': Cash_Flow,
-            'Cash_to_Market_cap_ratio':Cash_to_Market_cap_ratio
-            }
-            
-            print( '%(Count)4d) %(Company)-20s : \
-                   Market Capatilization = %(Market_Cap)-10s \
-                   Total Debt = %(Total_Debt)-8s \
-                   Total Assets = %(Total_Asset)-8s \
-                   Cash Flow = %(Cash_Flow)-10s \
-                   Ratio = %(Cash_to_Market_cap_ratio)-10s' \
-                   % result
-                   )
-        else:
-            print( '***** Error Reading page %s' % (r'http://www.moneycontrol.com/%s' % stocks['href']) )
-            
 
 
